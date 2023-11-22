e0d2807ba92ab6821e9b874d04573a9e0fe3c8d4
Mako <3201154@kuis.ac.jp>
2023-11-23 01:09:09 +0900

    chapter1.py

---
+++ chapter1.py
@@ -0,0 +1,17 @@
+print(30 + 20)
+a = 10
+
+print(90 / 2 )
+print( 10 * ( 7 - 2) + 5 )
+
+print('hello, yall')
+print('sorry')
+print("howdy, y'll")
+
+print ('this' 'could be')
+print('this', 'could be')
+
+
+
+
+
